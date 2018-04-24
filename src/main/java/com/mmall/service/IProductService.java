6b1fb7d51104765621489adb83c913be75046e61
RookieWangZhiWei <RookieWangZhiWei@163.com>
2018-04-25 00:08:24 +0800

    产品

---
+++ src/main/java/com/mmall/service/IProductService.java
@@ -0,0 +1,22 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.vo.ProductDetailVo;
+
+/**
+ * Created by RookieWangZhiWei on 2018/4/24.
+ */
+public interface IProductService {
+
+    ServerResponse saveOrUpdateProduct(Product product);
+
+    ServerResponse<String> setSaleStatus(Integer productId,Integer status);
+
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+
+    ServerResponse getProductList(int pageNum,int pageSize);
+
+    ServerResponse<PageInfo> searchProduct(String productName,Integer productId,int pageNum,int pageSize);
+}
---
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -0,0 +1,50 @@
+package com.mmall.service.impl;
+
+import com.mmall.service.IFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import
+
+/**
+ * Created by RookieWangZhiWei on 2018/4/24.
+ */
+public class FileServiceImpl implements IFileService {
+
+    private Logger logger = (Logger) LoggerFactory.getLogger(FileServiceImpl.class);
+
+    public String upload(MultipartFile file, String path) {
+        String fileName = file.getOriginalFilename();
+
+        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+        String uploadFileName = UUID.randomUUID().toString() + "." + fileExtensionName;
+
+        logger.info("开始上传文件,上传文件的文件名:{},上传的路径:{},新文件名:{}", fileName, path, uploadFileName);
+
+        File fileDir = new File(path);
+        if (!fileDir.exists()) {
+            fileDir.setWritable(true);
+            fileDir.mkdirs();
+        }
+        File targetFile = new File(path, uploadFileName);
+
+        try {
+            file.transferTo(targetFile);
+
+
+
+
+        } catch (IOException e) {
+            logger.info("上传文件异常", e);
+            return null;
+        }
+
+        return targetFile.getName();
+    }
+}
